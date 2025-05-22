3ea1e866f10b97c4c55c2bc9862ca4d24688c1c6
MechaniX05 <charleschristian.taneo@wlcormoc.edu.ph>
2025-05-23 00:31:01 +0800

    Add lib/widgets/bottom_nav_bar.dart 1747931461421

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: currentIndex,
+      onDestinationSelected: onTap,
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.home_outlined),
+          selectedIcon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.video_camera_back_outlined),
+          selectedIcon: Icon(Icons.video_camera_back),
+          label: 'Monitor',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.calendar_today_outlined),
+          selectedIcon: Icon(Icons.calendar_today),
+          label: 'Schedule',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.settings_outlined),
+          selectedIcon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+    );
+  }
+}
