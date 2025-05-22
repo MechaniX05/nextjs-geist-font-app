cc0c1ecb5675a856eb84f5f4789f79b858db3c2b
MechaniX05 <charleschristian.taneo@wlcormoc.edu.ph>
2025-05-23 00:31:05 +0800

    Add lib/providers/theme_provider.dart 1747931465393

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkMode = false;
+  final String _key = "theme_mode";
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeProvider() {
+    _loadThemeMode();
+  }
+
+  Future<void> _loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_key) ?? false;
+    notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_key, _isDarkMode);
+    notifyListeners();
+  }
+}
