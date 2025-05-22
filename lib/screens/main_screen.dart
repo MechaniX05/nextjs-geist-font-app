c93a94c4dc1b05f1e7462f1e5159eee851291b21
MechaniX05 <charleschristian.taneo@wlcormoc.edu.ph>
2025-05-23 00:30:55 +0800

    Add lib/screens/main_screen.dart 1747931455460

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../widgets/bottom_nav_bar.dart';
+import 'home_screen.dart';
+import 'monitoring_screen.dart';
+import 'settings_screen.dart';
+import 'schedule_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const MonitoringScreen(),
+    const ScheduleScreen(),
+    const SettingsScreen(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+      ),
+    );
+  }
+}
